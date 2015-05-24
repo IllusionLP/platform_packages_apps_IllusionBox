5f85469fed2e13442b3d1352b1e976239a2ef0f9
droyd <mikew8978@gmail.com>
2015-05-24 06:16:01 -0400

    Settings: Slims navbar customizations
    Refractor for IllusionBox/OwlsNest for aosip
    
    Original commit https://github.com/SlimRoms/packages_apps_Settings/commit/9d638$
    
    patchset 14: fix reset button in button style screen
    patchset 15: CheckBoxPreference to SwitchPreference
    patchset 16-24: refer to correct reset icon
    patchset 21: fix navigation dashboard icon
    
    add back our navbar tweeks. This is a completly overworked
    version compared to the one we carry about a year now. Small glitches
    we had are now all fixed. As well it does not need to register extra observers
    code was much slimmed down and improved.
    Beside that we adapt the 4.4 navbar changes to our system so that it it
    consistent with the new 4.4 UI
    This patch adds:
    - Navbar up to 5 buttons
    - dynamic menu behaviour
    - landscape navbar options as well now on tablets
    - button glow color
    - button icon color
    - navrign and navbar user can set system or custom icons
    - navbar height and width
    - enable/disable navbar
    - actions are vor navbar and available for the whole system over SlimActions.java
    	back
    	custom app or activtiy
    	home
    	kill app
    	last app
    	notification drawer
    	ime switcher
    	power menu
    	qs pannel
    	recents screen
    	screen off
    	screenshot
    	search
    	torch if it is available on the device
    - added Color class based on blk_jacks idea which first grey an icon out
      and then color it. This gives way better results. As well it fits now
      perfectly with the new 4.4 UI. Put it on a central place so that it is
      from everywhere available. notification shortcuts adapted to the new class
      place
    - complete new icons thanks to kroz who has redone all for xxhdpi
    - our slim special swipe drag drop press longpress options support
      like on 4.3 this two features
    - a lot lot fixes as well on current google code
    
    Conflicts:
    	res/values-h480dp/dimens.xml
    	res/values/slim_arrays.xml
    	res/values/slim_strings.xml
    	res/xml/settings_headers.xml
    	src/com/android/settings/slim/dslv/ButtonsListViewSettings.java
    	src/com/android/settings/slim/fragments/NotificationsShortcutFragment.java
    
    Change-Id: Ic000e81c9e3a191ba676632f4fab99d3007bde77
    
    Original commit https://github.com/SlimRoms/packages_apps_Settings/commit/9d638a94fc6af5df973651ecb503de543a4c25d8

--- src/com/illusion/box/SettingsActivity.java
+++ src/com/illusion/box/SettingsActivity.java
@@ -50,6 +50,7 @@ import com.android.settings.notification.ZenModeSettings;
 import com.android.settings.notificationlight.NotificationLightSettings;
 import com.android.settings.notificationlight.BatteryLightSettings;
 import com.android.settings.euphoria.VolumeSteps;
+import com.android.settings.slim.dslv.ActionListViewSettings;
 
 import com.illusion.box.fragments.*;
 import com.illusion.box.fragments.bar.*;
@@ -59,6 +60,7 @@ import com.illusion.box.fragments.display.*;
 import com.illusion.box.fragments.lock.*;
 import com.illusion.box.fragments.qs.*;
 import com.illusion.box.fragments.interfase.*;
+import com.illusion.box.fragments.navigation.*;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -237,6 +239,11 @@ public class SettingsActivity extends PreferenceActivity {
         InterfaceHolderSettings.class.getName(),
         VolumeSteps.class.getName(),
         AppSidebar.class.getName(),
+        NavigationHolderSettings.class.getName(),
+        NavBarButtonStyle.class.getName(),
+        NavbarSettings.class.getName(),
+        NavbarStyleDimenSettings.class.getName(),
+        ActionListViewSettings.class.getName(),
     };
 
     @Override
